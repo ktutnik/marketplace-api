712cd1c7e413f195001f1f8ba385a0a521dcc626
Ketut Sandiarsa <ktutnik@gmail.com>
2021-03-17 18:05:43 +0800

    Revert shop

--- src/api/shop/shop-entity.ts
+++ src/api/shop/shop-entity.ts
@@ -1,9 +1,9 @@
 import { bind, preSave, val } from "@plumier/core";
 import { genericController } from "@plumier/generic-controller";
 import { JwtClaims } from "plumier";
-import { Column, Entity, getRepository, ManyToOne, OneToMany } from "typeorm";
+import { Column, Entity, getRepository, OneToMany } from "typeorm";
 import { Item } from "../shop-item/shop-item-entity";
-import { User } from "../user/user-entity";
+import { ShopUser } from "../shop-user/shop-user-entity";
 import { EntityBase } from "../_shared/entity-base";
 
 @genericController(c => {
@@ -22,17 +22,4 @@ export class Shop extends EntityBase {
     @OneToMany(x => Item, x => x.shop)
     items: Item[]
 
-}
-
-export class ShopUser extends EntityBase {
-
-    @val.required()
-    @ManyToOne(x => User)
-    user: User
-
-    @ManyToOne(x => Shop)
-    shop:Shop
-
-    @Column({ default: "ShopStaff" })
-    role: "ShopOwner" | "ShopStaff"
 }
